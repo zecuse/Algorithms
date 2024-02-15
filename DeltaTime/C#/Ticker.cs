1a8112fe246609181ab45e71a87423b6128bfe59
Cory Williamson <zekos@ymail.com>
2024-02-14 23:47:00 -0500

    Added the Ticker and Updatable classes.

---
+++ DeltaTime/C#/Ticker.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+
+namespace DeltaTime;
+
+/// <summary>
+/// A class analogous to the OnUpdate loop often found in game engines.<br/>
+/// This is meant to be driven from another loop.
+/// </summary>
+public class Ticker()
+{
+	/// <summary>
+	/// All of the <see cref="Updatable"/> instances that will be updated each iteration.
+	/// </summary>
+	public List<Updatable> Updatables { get; set; } = [];
+
+	/// <summary>
+	/// A <see cref="Stopwatch"/> used to control the calculation of a deltaTime.<br/>
+	/// This is based on the number of ticks as counted by the system<br/>
+	/// and the frequency of the clock used by <see cref="Stopwatch"/>.
+	/// </summary>
+	Stopwatch Timer { get; } = new();
+
+	/// <summary>
+	/// The time at the end of the last <see cref="PerformTick"/> iteration.
+	/// </summary>
+	TimeSpan PrevTime { get; set; } = TimeSpan.Zero;
+
+	/// <summary>
+	/// Creates an offset from the current time to jump the <see cref="Updatables"/>.<br/>
+	/// If an <see cref="Updatable"/> consistently runs exactly the same over an hour of time, an offset of<br/>
+	/// 45 minutes at the beginning of this hour will instantaneously fast forward 45 minutes,<br/>
+	/// provided this Updatable properly handles updating with such a large deltaTime.
+	/// </summary>
+	TimeSpan OffsetTime { get; set; } = TimeSpan.Zero;
+
+	/// <summary>
+	/// Determine if the <see cref="Stopwatch"/> is currently ticking.
+	/// </summary>
+	/// <returns><see langword="true"/> if the <see cref="Timer"/> is currently running, <see langword="false"/> otherwise.</returns>
+	public bool IsRunning() => Timer.IsRunning;
+
+	/// <summary>
+	/// Start or resume the <see cref="Stopwatch"/>.
+	/// </summary>
+	public void Start() => Timer.Start();
+
+	/// <summary>
+	/// Stop the <see cref="Stopwatch"/>, reset its ticks to 0, and start it immediately.
+	/// </summary>
+	public void Restart() => Timer.Restart();
+
+	/// <summary>
+	/// Stop the <see cref="Stopwatch"/>.
+	/// </summary>
+	public void Pause() => Timer.Stop();
+
+	/// <summary>
+	/// Stop the <see cref="Stopwatch"/> and reset its ticks to 0.
+	/// </summary>
+	public void Reset() => Timer.Reset();
+
+	/// <summary>
+	/// Get the current elapsed time of the <see cref="Timer"/> as a <see cref="TimeSpan"/>.
+	/// </summary>
+	/// <returns>A <see cref="TimeSpan"/> representing the elapsed since the last <see cref="Start"/> or <see cref="Restart"/>.</returns>
+	public TimeSpan GetTime() => Timer.Elapsed;
+
+	/// <summary>
+	/// Change the deltaTime for the next iteration of <see cref="PerformTick"/>.<br/>
+	/// A positive <see cref="TimeSpan"/> moves time forward and negative backward.
+	/// </summary>
+	/// <param name="offset">A <see cref="TimeSpan"/> representing how much time should change.</param>
+	public void Offset(TimeSpan offset) => OffsetTime = offset;
+
+	/// <summary>
+	/// Add an <see cref="Updatable"/> to begin receiving deltaTime updates.
+	/// </summary>
+	/// <param name="updatable">The <see cref="Updatable"/> to begin updating.</param>
+	public void ReceiveUpdatable(Updatable updatable)
+	{
+		if (Updatables.Count == 0)
+			Updatables.Add(updatable);
+		else if (Updatables.Last().CompareTo(updatable) < 0)
+			Updatables.Add(updatable);
+		else if (Updatables.First().CompareTo(updatable) > 0)
+			Updatables.Insert(0, updatable);
+		else
+		{
+			int idx = Updatables.BinarySearch(updatable);
+			if (idx < 0)
+				idx = ~idx;
+			Updatables.Insert(idx, updatable);
+		}
+	}
+
+	/// <summary>
+	/// Calculate a deltaTime and send it to all <see cref="Updatables"/>.<br/>
+	/// <see cref="OffsetTime"/> will be reset to <see cref="TimeSpan.Zero"/>.<br/>
+	/// <see cref="PrevTime"/> will receive the currently elapsed time AFTER all <see cref="Updatables"/> have been updated.
+	/// </summary>
+	public void PerformTick()
+	{
+		TimeSpan now = Timer.Elapsed;
+		double deltaTime = now.TotalMilliseconds - PrevTime.TotalMilliseconds + OffsetTime.TotalMilliseconds;
+		OffsetTime = TimeSpan.Zero;
+
+		foreach (var updatable in Updatables)
+			updatable.Update(deltaTime);
+		PrevTime = Timer.Elapsed;
+	}
+}
---
+++ DeltaTime/C#/Updatable.cs
@@ -0,0 +1,36 @@
+﻿namespace DeltaTime;
+
+/// <summary>
+/// An abstract class to identify anything that can be updated with a deltaTime.
+/// </summary>
+public abstract class Updatable : IComparable<Updatable>
+{
+	/// <summary>
+	/// Determines when any Updatable should be updated before or after any other.
+	/// </summary>
+	protected virtual int Order { get; } = 10;
+
+	/// <summary>
+	/// Defines how an Updatable will respond to receiving a deltaTime.
+	/// </summary>
+	/// <param name="deltaTime">A, usually small and less than 1, value in total milliseconds between tick iterations.</param>
+	public abstract void Update(double deltaTime);
+
+	/// <summary>
+	/// Sort all Updatables with respect to their <see cref="Order"/>.
+	/// </summary>
+	/// <param name="that">Another Updatable.</param>
+	/// <returns>If one of the following is true:<br/>
+	/// <code>
+	/// 1. Negative: <see langword="this"/> updatable should precede <see href="that"/><br/>
+	/// 2. Zero:     <see langword="this"/> updatable is logically equivalent to <see href="that"/>, order undefined<br/>
+	/// 3. Positive: <see langword="this"/> updatable should procede <see href="that"/><br/>
+	/// 4. Null:     <see langword="this"/> always precedes a null <see href="that"/>
+	/// </code></returns>
+	public int CompareTo(Updatable? that)
+	{
+		if (that == null)
+			return -1;
+		return this.Order.CompareTo(that.Order);
+	}
+}
