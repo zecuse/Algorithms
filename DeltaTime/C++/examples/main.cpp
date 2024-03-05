ef4e210683b29e495d9bee9ba02b0defb268fc0d
Cory Williamson <zekos@ymail.com>
2024-03-04 22:02:49 -0500

    Adjusted main to create a control thread and worker thread. Queue will be added later.

--- DeltaTime/C++/examples/main.cpp
+++ DeltaTime/C++/examples/main.cpp
@@ -1,14 +1,47 @@
+#include <Concurrent/Queue.hpp>
 #include <DeltaTime/Ticker.hpp>
 #include <iostream>
+#include <stop_token>
+#include <thread>
 
 using namespace std;
-using namespace DeltaTime;
+
+void UserInput(stop_source src)
+{
+	cout << "user input started\n";
+	string input;
+	while (getline(cin, input))
+	{
+		if (input == "quit")
+		{
+			src.request_stop();
+			break;
+		}
+		cout << "getting next input\n";
+	}
+	cout << "user input ended" << endl;
+}
+
+void DoTicking(stop_token tkn)
+{
+	cout << "ticking started\n";
+	while (true)
+	{
+		if (tkn.stop_requested())
+		{
+			break;
+		}
+		cout << "tick\n";
+		this_thread::sleep_for(1s);
+	}
+	cout << "ticking ended" << endl;
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	Updatable up;
-	Ticker ti;
-	ti.ReceiveUpdatable(up);
+	stop_source source;
+	jthread ui(UserInput, source);
+	jthread tick(DoTicking, source.get_token());
+	cout << "[main] bye!!" << endl;
 	return 0;
 }
