cde59ee2b090bf955e2416aaa88653b71be277ca
Cory Williamson <zekos@ymail.com>
2024-03-19 00:00:37 -0400

    Modified Queue so push will also try_aquire instead of hang until free.

--- DeltaTime/C++/Concurrent/Queue.hpp
+++ DeltaTime/C++/Concurrent/Queue.hpp
@@ -8,27 +8,24 @@ namespace Concurrent
 template<class T>
 class Queue
 {
-	bool awake = false;
 	std::queue<T> que = {};
-	std::binary_semaphore sema = { 0 };
+	std::binary_semaphore sema { 1 };
 public:
-	void Awake()
-	{
-		if (!awake)
-			sema.release();
-		awake = true;
-	}
-
-	void push(const T &item)
-	{
-		sema.acquire();
-		que.push(item);
-		sema.release();
-	}
-
-	bool pop(T &item, std::chrono::duration<double, std::chrono::milliseconds> timeout)
+	bool push(const T &item, std::chrono::duration<double, std::milli> timeout)
 	{
 		if (sema.try_acquire_for(timeout))
+		{
+			que.push(item);
+			sema.release();
+			return true;
+		}
+
+		return false;
+	}
+
+	bool pop(T &item, std::chrono::duration<double, std::milli> timeout)
+	{
+		if (!que.empty() && sema.try_acquire_for(timeout))
 		{
 			item = que.front();
 			que.pop();
